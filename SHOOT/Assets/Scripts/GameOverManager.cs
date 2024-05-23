434e64f0ee300327229e0c88200ce1a6af9b4d68
OV3RL0RDzr <z.ruckebusch@rubika-edu.com>
2024-05-23 12:57:53 +0200

    aez

--- SHOOT/Assets/Scripts/GameOverManager.cs
+++ SHOOT/Assets/Scripts/GameOverManager.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOverManager : MonoBehaviour
 {
     public GameObject gameOverMenu;
     private PlayerHealth playerHealth;
+    public TMPro.TextMeshProUGUI FinalScoreText;
 
     void Start()
     {
@@ -32,5 +35,10 @@ public class GameOverManager : MonoBehaviour
     {
         SceneManager.LoadScene("MainMenu");
         Time.timeScale = 1;
+    }
+
+    void TheFinalScoreText()
+    {
+        ScoreText.text = "Score Final: " + currentScore.ToString();
     }
 }
