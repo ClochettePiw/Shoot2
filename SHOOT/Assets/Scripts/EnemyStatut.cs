09e78ef94f26db38c560da7ec72841c0e99e54bf
ClochettePiw <Baudinetpr@gmail.com>
2024-05-23 14:06:55 +0200

    ratio

--- SHOOT/Assets/Scripts/EnemyStatut.cs
+++ SHOOT/Assets/Scripts/EnemyStatut.cs
@@ -6,5 +6,10 @@ public class EnemyStatut : MonoBehaviour
 {
     public bool retweetMob;
     public int enemyScore;
-    
+   
+    public IEnumerator Destruction()
+    {
+        yield return new WaitForSeconds(2);
+        Destroy(gameObject);
+    }
 }
